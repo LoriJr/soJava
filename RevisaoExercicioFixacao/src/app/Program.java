2a1e10e8a112bc1fc333a963778efaf1d5d13681
lorijr <jrsilva.lorivas@gmail.com>
2025-06-22 15:32:07 -0300

    program add input data - created class services: contract and onlinePayment

--- RevisaoExercicioFixacao/src/app/Program.java
+++ RevisaoExercicioFixacao/src/app/Program.java
@@ -1,9 +1,37 @@
 package app;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		
+		System.out.println("Entre com os dados do contrato");
+		System.out.print("Número: ");
+		int contractNumber = sc.nextInt();
+		sc.nextLine();
+		System.out.print("data (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.nextLine(), dtf);
+		System.out.print("Valor do contrato: ");
+		double valor = sc.nextDouble();
+		System.out.print("Número de parcelas: ");
+		int numParc = sc.nextInt();
+		
+		Contract contrato = new Contract(contractNumber, date, valor);
+		
+		
+		
+		sc.close();
 
 	}
 
